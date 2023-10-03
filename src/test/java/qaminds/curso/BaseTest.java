cd37768e43dca235a41c0990820d3e55e781aad2
Braulio <braulio-03@live.com.mx>
2023-10-03 10:29:42 -0600

    primer commit

---
+++ src/test/java/qaminds/curso/BaseTest.java
@@ -0,0 +1,29 @@
+package qaminds.curso;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+public class BaseTest {
+   WebDriver driver;
+    @BeforeTest
+    public void openBrowser() throws InterruptedException {
+
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://microsoft.com");
+        driver.manage().window().maximize();
+        Thread.sleep(15000);
+    }
+/*@AfterTest
+public void cerrarBrowser(){
+        driver.quit();
+}*/
+
+public WebDriver getDriver(){
+        return driver;
+}
+
+}
---
+++ src/test/java/qaminds/curso/header/HeaderTest.java
@@ -0,0 +1,39 @@
+package qaminds.curso.header;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import qaminds.curso.BaseTest;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class HeaderTest extends BaseTest {
+
+    @Test(testName = "Validar el header de Microsoft", description = "Validar que existan los elementos del header en la pagina principal de Microsofts")
+
+    public void validateHeader() {
+        String[] menuList = {"Microsoft 360", "Teams", "Windows", "Surface", "Xbox", "Soporte"};
+        WebElement menuElement = getDriver().findElement(By.cssSelector("uhf-g-nav > ul"));
+        List<WebElement> myList = menuElement.findElements(By.tagName("a"));
+        //List<String> myList = menuElement.findElements(By.tagName("a")).stream().map(WebElement::getText)
+        //      .collect(Collectors.toList());
+
+        List<String> newList = new ArrayList<>();
+        for (WebElement element : myList)
+            newList.add(element.getText());
+
+        boolean exist = false;
+        for (int i = 0; i < menuList.length; i++) {
+            for (int x = 0; x < newList.size(); x++) {
+                if (newList.get(x).equals(menuList[i])) {
+                    exist = true;
+                }
+            }
+
+            Assert.assertTrue(exist, "El elemento " + menuList[i] + "No se encontro la pagina. ");
+        }
+    }
+}
